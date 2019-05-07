cbfbd4b2df1dc46eed0c4b02966522ae87fb4ab9
Ovdin Kirill <Kirill.Ovdin@infotecs.ru>
2019-05-07 15:08:17 +0300

    day05 25%

---
+++ day05/ex01/ex01.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS ft_table (	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+										login VARCHAR(8) NOT NULL DEFAULT 'toto',
+										`group` ENUM ('staff', 'student', 'other') NOT NULL,
+										creation_date DATE NOT NULL);
